5d907339d17822a8b6e80d69400728d73cefb6a9
WondeMathe <127203235+WondeMathe@users.noreply.github.com>
2023-12-13 19:59:45 +0300

    Calculator.cpp
    This is a simple calculator using a switch

--- IO.cpp
+++ IO.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 using namespace std;
-int main(){
-   cout<<"Hi, this is from branch";
-
-
-return 0;
+int main()
+{
+ float num1, num2, result;
+ char op;
+ cout<<"Enter first number: ";
+ cin>>num1;
+ cout<<"Enter operator: ";
+ cin >>op;
+ cout<<"Enter second number: ";
+ cin>>num2;
+ switch(op){
+  case '+': cout<<num1+num2;
+  break;
+     case '-': cout<<num1-num2;
+  break;
+  case '*': cout<<num1*num2;
+  break;
+  case '/': cout<<num1/num2;
+  break;
+  default: cout<<"Invalid operator";
+ }
+ 
+ return 0; 
 }
