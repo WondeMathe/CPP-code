bed8c854f4f4e50492cda1d06becb2d97348e44b
Wondwosen Mathewos <127203235+WondeMathe@users.noreply.github.com>
2024-06-01 20:40:23 +0300

    Add files via upload

---
+++ Sort three integers.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+void order(int* n1, int* n2, int* n3){
+
+if(*n1>*n2){
+int temp = *n1;
+*n1 = *n2;
+*n2 = temp;
+}
+
+if(*n2>*n3){
+int temp = *n2;
+*n2 = *n3;
+*n3 = temp;
+}
+
+if(*n1>*n3){
+int temp = *n1;
+*n1 = *n3;
+*n3 = temp;
+}
+if(*n2>*n3){
+int temp = *n2;
+*n2 = *n3;
+*n3 = temp;
+}
+}
+
+int main(){
+
+int num1, num2, num3;
+
+cout<<"Enter three numbers: ";
+cin>>num1>>num2>>num3;
+
+int* n1 = &num1;
+int* n2 = &num2;
+int* n3 = &num3;
+
+order(n1, n2, n3);
+
+cout<<*n1<<" "<<*n2<<" "<<*n3<<endl;
+
+return 0;
+
+}
