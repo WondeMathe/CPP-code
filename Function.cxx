7ac3bffd041d2df784a0325cb65f1cb93d6c9a33
Wondwosen Mathewos <127203235+WondeMathe@users.noreply.github.com>
2024-06-18 19:25:59 +0300

    Add function of cpp

---
+++ Function.cxx
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+//Adding two numbers
+int add_numbers(int, int);
+
+int main()
+{
+	int firstNum, secondNum, result;
+	cin>>firstNum>>secondNum;
+	result= add_numbers(firstNum, secondNum);
+	cout<<result;
+	
+}
+//Defining addtion function
+int add_numbers(int n1, int n2){
+int total=0, n1=4, n2=2;
+total=n1+n2;
+return total;
+}
